6f73a1e0723328afb4b5c247ec4ea32d76991e14
YA Chang <yhchang1213@gmail.com>
2024-11-05 23:45:33 +0800

    Initial Commit

---
+++ iOSTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSTest
+//
+//  Created by 張雅惠 on 2024/11/5.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ iOSTestTests/iOSTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  iOSTestTests.swift
+//  iOSTestTests
+//
+//  Created by 張雅惠 on 2024/11/5.
+//
+
+import Testing
+@testable import iOSTest
+
+struct iOSTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
